7741d94d6f1f8360218a9a1a0cf40d063d274d0e
ncp204 <giodongnamayv3@gmail.com>
2021-05-23 09:59:27 +0700

    lam trang result

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,18 +3,18 @@ import Menu from './components/Menu'
 import Footer from './components/Footer';
 import RounterURL from './rounter/RounterURL';
 import Schedule from './components/Schedule';
-import Technology from './components/Technology';
 import Technical from './components/Technical';
 import About from './components/About';
 import Member from './components/Member';
 import Contact from './components/Contact';
 import Home from './components/Home';
 import Project from './components/Project';
+import Result from './components/Result';
 
 const sections = [
   {id:1, component: Home , title: 'Home', url: '/'},
   {id:2, component: Schedule, title: 'Schedule', url: '/schedule'},
-  {id:3, component: Technology, title: 'Technology', url: '/technology'},
+  {id:3, component: Result, title: 'Result', url: '/result'},
   {id:4, component: Technical, title: 'Technical', url: '/technical'},
   {id:5, component: About, title: 'About', url: '/about'},
   {id:6, component: Member, title: 'Member', url: '/member'},
---
+++ webapp/src/components/Result.js
@@ -0,0 +1,221 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+    render() {
+        return (
+            <div>
+                <section>
+                    <h1 className="result-page"><b>RESULT</b></h1>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-6 col-md-4">
+                                <div className="card-container-imagia">
+                                    <div className="card-imagia">
+                                        <div className="front-imagia">
+                                            <div className="cover-imagia"><img alt="" src="https://unsplash.it/720/500?image=1067" /></div>
+                                            <div className="user-imagia"><img className="img-circle" alt="" src="assets/img/team/thinhngu.jpg" /></div>
+                                            <div className="content-imagia">
+                                                <h3 className="name-imagia">Lê Quốc Thịnh</h3>
+                                                <p className="subtitle-imagia">MSSV: 19130215</p>
+                                            </div>
+                                            <div className="footer-imagia"><span><i className="fa fa-plus" /> Di chuyển vào thẻ để xem đánh giá</span></div>
+                                        </div>
+                                        <div className="back-imagia">
+                                            <div className="content-imagia content-back-imagia">
+                                                <div>
+                                                    <h4>Trang web Project</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Trang web Nhóm</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Đánh giá</h4>
+                                                    <ul>
+                                                        <li>Hoàn thành: 100%</li>
+                                                        <li>Đóng góp: 20%</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div className="footer-imagia">
+                                                <a class="btn btn-primary" role="button">Đánh giá cá nhân</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-6 col-md-4">
+                                <div className="card-container-imagia">
+                                    <div className="card-imagia">
+                                        <div className="front-imagia">
+                                            <div className="cover-imagia cover-gradient" />
+                                            <div className="user-imagia"><img className="img-circle" alt="" src="assets/img/team/tanloc.jpg" /></div>
+                                            <div className="content-imagia">
+                                                <h3 className="name-imagia">Cao Huy Tấn Lộc</h3>
+                                                <p className="subtitle-imagia">MSSV: 19130120</p>
+                                            </div>
+                                            <div className="footer-imagia"><span><i className="fa fa-plus" /> Di chuyển vào thẻ để xem đánh giá</span></div>
+                                        </div>
+                                        <div className="back-imagia">
+                                            <div className="content-imagia content-back-imagia">
+                                                <div>
+                                                    <h4>Trang web Project</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Trang web Nhóm</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Đánh giá</h4>
+                                                    <ul>
+                                                        <li>Hoàn thành: 100%</li>
+                                                        <li>Đóng góp: 20%</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div className="footer-imagia">
+                                                <a class="btn btn-primary" role="button">Đánh giá cá nhân</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-6 col-md-4">
+                                <div className="card-container-imagia">
+                                    <div className="card-imagia">
+                                        <div className="front-imagia">
+                                            <div className="cover-imagia"><img alt="" src="https://unsplash.it/720/500?image=1067" /></div>
+                                            <div className="user-imagia"><img className="img-circle" alt="" src="assets/img/team/138299251_440090247175658_4216634827208895764_n.jpg" /></div>
+                                            <div className="content-imagia">
+                                                <h3 className="name-imagia">Nguyễn Dũy Long</h3>
+                                                <p className="subtitle-imagia">MSSV: 19130128</p>
+                                            </div>
+                                            <div className="footer-imagia"><span><i className="fa fa-plus" /> Di chuyển vào thẻ để xem đánh giá</span></div>
+                                        </div>
+                                        <div className="back-imagia">
+                                            <div className="content-imagia content-back-imagia">
+                                                <div>
+                                                    <h4>Trang web Project</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Trang web Nhóm</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Đánh giá</h4>
+                                                    <ul>
+                                                        <li>Hoàn thành: 100%</li>
+                                                        <li>Đóng góp: 20%</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div className="footer-imagia">
+                                                <a class="btn btn-primary" role="button">Đánh giá cá nhân</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row row2-result">
+                            <div className="col-sm-6 col-md-4">
+                                <div className="card-container-imagia">
+                                    <div className="card-imagia">
+                                        <div className="front-imagia">
+                                            <div className="cover-imagia cover-gradient" />
+                                            <div className="user-imagia"><img className="img-circle" alt="" src="assets/img/team/z2418551337261_1a7021614956dab0f80794d7f4d18401.jpg" /></div>
+                                            <div className="content-imagia">
+                                                <h3 className="name-imagia">Lương Hữu Luân</h3>
+                                                <p className="subtitle-imagia">MSSV: 19130131</p>
+                                            </div>
+                                            <div className="footer-imagia"><span><i className="fa fa-plus" /> Di chuyển vào thẻ để xem đánh giá</span></div>
+                                        </div>
+                                        <div className="back-imagia">
+                                            <div className="content-imagia content-back-imagia">
+                                                <div>
+                                                    <h4>Trang web Project</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Trang web Nhóm</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Đánh giá</h4>
+                                                    <ul>
+                                                        <li>Hoàn thành: 100%</li>
+                                                        <li>Đóng góp: 20%</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div className="footer-imagia">
+                                                <a class="btn btn-primary" role="button">Đánh giá cá nhân</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-6 col-md-4">
+                                <div className="card-container-imagia">
+                                    <div className="card-imagia">
+                                        <div className="front-imagia">
+                                            <div className="cover-imagia cover-gradient" />
+                                            <div className="user-imagia"><img className="img-circle" alt="" src="assets/img/team/175707224_205212954495101_3489952911742350797_n.jpg" /></div>
+                                            <div className="content-imagia">
+                                                <h3 className="name-imagia">Nguyễn Công Phúc</h3>
+                                                <p className="subtitle-imagia">MSSV: 19130172</p>
+                                            </div>
+                                            <div className="footer-imagia"><span><i className="fa fa-plus" /> Di chuyển vào thẻ để xem đánh giá</span></div>
+                                        </div>
+                                        <div className="back-imagia">
+                                            <div className="content-imagia content-back-imagia">
+                                                <div>
+                                                    <h4>Trang web Project</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Trang web Nhóm</h4>
+                                                    <ul>
+                                                        <li></li>
+                                                        <li></li>
+                                                        <li></li>
+                                                    </ul>
+                                                    <h4>Đánh giá</h4>
+                                                    <ul>
+                                                        <li>Hoàn thành: 100%</li>
+                                                        <li>Đóng góp: 20%</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div className="footer-imagia">
+                                                <a class="btn btn-primary" role="button">Đánh giá cá nhân</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Result;
