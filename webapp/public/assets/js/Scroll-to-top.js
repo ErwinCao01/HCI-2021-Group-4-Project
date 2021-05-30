4f87069addb562061f15a2e6ba7ea7a517fd53ef
ncp204 <giodongnamayv3@gmail.com>
2021-05-30 22:16:14 +0700

    chỉnh sửa lần 5 ngày 30/5/2021

--- webapp/public/assets/js/Scroll-to-top.js
+++ webapp/public/assets/js/Scroll-to-top.js
@@ -1,9 +1,4 @@
 jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "back to top" link is shown
-	var offset = 300,
-		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		offset_opacity = 1200,
-		//duration of the top scrolling animation (in ms)
 		scroll_top_duration = 700,
 		//grab the "back to top" link
 		$back_to_top = $('.cd-top');
